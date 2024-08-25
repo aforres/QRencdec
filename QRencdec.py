f3685260d0753462dc5f4f32e43c16ada6ab7084
aforres <aforres@users.noreply.github.com>
2024-08-26 09:49:03 +1000

    Update QRencdec.py

--- QRencdec.py
+++ QRencdec.py
@@ -1,10 +1,5 @@
 import streamlit as st
 import qrcode
-# from PIL import Image
-# from PIL import Image 
-# from Pillow import Image 
-# Pillow added 20240824
-# import PIL #Image
 import cv2 as cv
 
 st.header("QRcodePlus - Run Encode and then Decode")
