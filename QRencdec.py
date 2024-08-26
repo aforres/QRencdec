4b162ccf852d0dea41f4e0896658c9a9d81f640e
aforres <aforres@users.noreply.github.com>
2024-08-26 13:34:57 +1000

    Update QRencdec.py

--- QRencdec.py
+++ QRencdec.py
@@ -19,8 +19,8 @@ def testfn():
     
     img = qr.make_image(fill_color="black",
                         back_color="white").convert('RGB')
-    img.save("./QRencdec/image_folder/sample.png")
-    st.image("./QRencdec/image_folder/sample.png")
+    img.save("./QRencdec/image_folder/sample.png","w+b")
+    st.image("sample.png")
 
 
 if st.button('Click Me'):
